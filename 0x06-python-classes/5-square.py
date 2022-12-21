c6682ca767a316de10f6903b8e75e0ee8b789952
nick-misomi <mwikasomi@gmail.com>
2022-12-21 11:39:12 -0800

    Hot fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -19,7 +19,7 @@ class Square:
         """Get/set the current size of the square."""
         return (self.__size)
 
-    @size.sette
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
